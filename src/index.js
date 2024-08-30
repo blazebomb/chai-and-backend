5bf0fd9eaccf77422e64e77e0e6b045340a4d7e5
blazebomb <ashishkumar850601@gmail.com>
2024-08-30 22:11:24 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// require('dotenv').config({ path: './.env' });
+
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+
+dotenv.config({
+     path: './.env' 
+});
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+import expess from 'express';
+(async () => {
+   try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("errror", error);
+            throw error     
+        })
+
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+   } catch (error) {
+        console.log("error", error);
+        throw err
+   }
+})()
+*/
